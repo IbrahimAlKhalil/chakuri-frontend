d6a2c301255ae6650d3c2bbd75a1436720d767ce
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2020-01-26 18:42:34 +0600

    Fix: menu not showing dashboard

--- src/modules/menu-items.js
+++ src/modules/menu-items.js
@@ -1,13 +1,13 @@
 export const common = {
     notification: {
-        label: 'নোটিফিকেশন',
+        title: 'নোটিফিকেশন',
         link: 'notifications',
         icon: 'fa fa-bell',
         badge: 0
     },
 
     profile: {
-        label: 'প্রোফাইল',
+        title: 'প্রোফাইল',
         link: 'profile',
         icon: 'fa fa-user'
     }
@@ -15,17 +15,17 @@ export const common = {
 
 export const employee = [
     {
-        label: 'জীবন বৃত্তান্ত (CV)',
+        title: 'জীবন বৃত্তান্ত (CV)',
         link: 'resume',
         icon: 'el-icon-s-custom',
     },
     {
-        label: 'ফেভারিট',
+        title: 'ফেভারিট',
         link: 'favorites',
         icon: 'fas fa-heart',
     },
     {
-        label: 'আবেদনকৃত চাকুরী সমূহ',
+        title: 'আবেদনকৃত চাকুরী সমূহ',
         link: 'applications',
         icon: 'fas fa-paper-plane'
     }
@@ -33,12 +33,12 @@ export const employee = [
 
 export const institution = [
     {
-        label: 'চাকুরীর বিজ্ঞাপন দিন',
+        title: 'চাকুরীর বিজ্ঞাপন দিন',
         link: 'post-job',
         icon: 'el-icon-s-marketing',
     },
     {
-        label: 'বিজ্ঞাপন সমূহ',
+        title: 'বিজ্ঞাপন সমূহ',
         link: 'jobs',
         icon: 'el-icon-s-order',
     },
